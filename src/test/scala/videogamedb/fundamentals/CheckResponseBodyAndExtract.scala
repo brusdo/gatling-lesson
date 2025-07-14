64ccc8887f9e852bcd68098dbe533d0d4a0354de
Bruna de Oliveira <bruna.oliveira@mercator.group>
2025-07-14 14:48:42 +0100

    Add Check Response Body with jsonPath

--- src/test/scala/videogamedb/fundamentals/CheckResponseBodyAndExtract.scala
+++ src/test/scala/videogamedb/fundamentals/CheckResponseBodyAndExtract.scala
@@ -12,7 +12,19 @@ class CheckResponseBodyAndExtract extends Simulation {
 
     .exec(http(requestName = "Get specific game")
     .get("/videogame/1")
-    .check(jsonPath("$.name").is(expected = "Resident Evil 4"))) // visit www.jsonpath.com and paste the jason expression $.name
+    .check(jsonPath(path ="$.name").is(expected = "Resident Evil 4"))) // visit www.jsonpath.com and paste the jason expression $.name
+
+    //Extract data from response for correlation
+    .exec(http(requestName = "Get all video games")
+    .get("/videogame")
+    .check(jsonPath(path = "$[1].id").saveAs(key = "gameId")))
+    .exec {session => println(session); session} // To debug, see what is inse '4gameId'
+
+    .exec(http(requestName= "Get specific game")
+    .get("/videogame/#{gameId}")
+    .check(jsonPath(path ="$.name").is(expected = "Gran Turismo 3"))
+    .check(bodyString.saveAs(key = "responseBody"))) // To debug, see the entire body response
+    .exec { session => println(session("responseBody").as[String]); session} // print body4
 
   setUp(
     scn.inject(atOnceUsers(users = 1))
