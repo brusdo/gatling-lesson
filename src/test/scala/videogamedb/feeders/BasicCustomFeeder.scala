9d9f9e5ce3c64d2a7f60931d8ce12245884dfd06
Bruna de Oliveira <bruna.oliveira@mercator.group>
2025-07-15 09:56:34 +0100

    Add Basic Custom Feeder

---
+++ src/test/scala/videogamedb/feeders/BasicCustomFeeder.scala
@@ -0,0 +1,32 @@
+package videogamedb.feeders
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class BasicCustomFeeder extends Simulation {
+
+  val httpProtocol = http.baseUrl(url = "https://videogamedb.uk/api")
+    .acceptHeader(value = "application/json")
+
+  val idNumbers = (1 to 10).iterator
+
+  val customFeeder = Iterator.continually(Map("gameId" -> idNumbers.next()))
+
+  def getSpecificVideoGame() = {
+    repeat(times = 10) {
+      feed(customFeeder)
+        .exec(http(requestName = "Get video game with id - #{gameId}")
+        .get("/videogame/#{gameId}")
+        .check(status.is(expected = 200)))
+        .pause(duration = 1)
+    }
+  }
+
+  val scn = scenario(name = "Basic Custom Feeder")
+    .exec(getSpecificVideoGame())
+
+  setUp(
+    scn.inject(atOnceUsers(users = 1))
+  ).protocols(httpProtocol)
+
+}
